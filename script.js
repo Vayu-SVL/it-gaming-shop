6834da25f9385d639460e258eb2bbc7e8fe6e631
Vayu-SVL <vayusoukvirai2006@gmail.com>
2026-05-27 01:40:27 +0700

    script.js

--- script.js
+++ script.js
@@ -979,6 +979,7 @@ function initLanguageSwitch() {
     menu.querySelectorAll('a').forEach(link => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
+            e.stopPropagation();
             const newLang = link.dataset.lang;
             if (newLang === currentLang) {
                 menu.classList.remove('show');
